7e2c1f14efa8c5c00cb9c82ff337268197b5d2e6
cryptobuds2022 <cryptobuds2022@gmail.com>
2022-10-05 11:20:01 +0200

    Initial upload after wallet fix

---
+++ httpdocs/ethw-test.js
@@ -0,0 +1,33 @@
+// deployed from 0xBEdc9a7220D2ea5c15C8d20B3FaBDbeCa29acAe0
+
+window.address = {};
+window.decimals = {};
+
+window.ETH = "ETHW";
+window.decimals['eth'] = 18;
+
+window.acceptedTokens = ["usdt", "usdc", "dai", "eth"]; // "eth" has to be the last one
+window.tokenName = ["USDT", "USDC", "DAI", window.ETH];
+
+window.address['usdt'] = '0xE3F5a90F9cb311505cd691a46596599aA1A0AD7D';
+window.decimals['usdt'] = 6;
+window.address['usdc'] = '0x765277EebeCA2e31912C9946eAe1021199B39C61';
+window.decimals['usdc'] = 6;
+window.address['dai'] = '0x639A647fbe20b6c8ac19E48E2de44ea792c62c5C';
+window.decimals['dai'] = 18;
+
+window.address['nft'] = '0xD7F68F4349527daE381233dAA120BB42e310C986';
+
+window.netName = "ethw-iceberg-testnet";
+window.rpcServer = 'https://iceberg.ethereumpow.org';
+window.netId = 10002;
+window.netId0x = '0x2712';
+window.blockExplorer = "http://iceberg.ethwscan.com";
+
+window.maxGasPerTx = 30000000;
+window.minGasCostE9 = 50;
+window.maxNftsPerTx = 40;
+
+window.nftMarketPlace = 'https://oasis-nft.dog/';
+window.nftRarity = 'https://nftrarity.dog/nft/'
+window.blockExplorer = "https://explorer.dogechain.dog/";
